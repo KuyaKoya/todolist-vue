52905647559a89119a9bfbc1048beb2f3d80105b
KuyaKoya <sethmcornelio@su.edu.ph>
2022-03-15 13:56:27 +0800

    moved text to the center of page

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -9,8 +9,13 @@
 
 <style scoped>
 .home {
-  display: flex;
-  justify-content: center;
-  align-content: center;
+  position: absolute;
+  top: 50%; margin-top:-100px;
+  font-weight: bold;
+  font-size: 16px;
+  text-transform: uppercase;
+  text-align:center;
+  width: 100%;
+  height: 100%;
 }
 </style>
